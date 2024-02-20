fd47080ea0eedbc85d5690d08a384764cc7f6bbd
Logan Cunningham <cunninlo@sheridancollege.ca>
2024-02-20 12:01:55 -0500

    Added my own class and comments

---
+++ src/Logan.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * @date 2024-02-20
+ * @author cunni
+ * These are the comments I am adding to my class as instructed
+ * My favourite genre to watch is comedy for sure
+ * I can't believe I got over 80 on part a!!!
+ */
+public class Logan {
+    
+}
